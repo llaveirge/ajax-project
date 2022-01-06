25a604654376085e75d020b156cf55142bd0e40c
Lindsey Laveirge <llaveirge@gmail.com>
2022-01-05 17:38:49 -0700

    Reset the form to clear the search input. #1

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@ var $searchInput = document.getElementById('search');
 var $searchForm = document.getElementById('form');
 
 // Listen for events and save search value to a variable before resetting the form:
-
 var query;
 
 function searchEventHandler(event) {
   event.preventDefault();
 
   query = $searchInput.value;
-  return query;
 
+  $searchForm.reset();
+
+  return query;
 }
 
 $searchForm.addEventListener('submit', searchEventHandler);
