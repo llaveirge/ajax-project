2c223bb4a52b40bb5056c08ae852a42bb23cbd7b
Lindsey Laveirge <llaveirge@gmail.com>
2022-01-05 18:58:33 -0700

    Add and call the 'getObjectIds' function in the 'submit' event handler, and make sure that the Met object IDs array containes the appropriate number of object IDs compared to those returned in Ajax response. #1

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
 var $searchInput = document.getElementById('search');
 var $searchForm = document.getElementById('form');
+// Emtpy array to store Met object IDs in once gotten from the API
+var objIdArr = [];
 
 // Listen for events and save search value to a variable before resetting the form:
 var query;
 
 function searchEventHandler(event) {
   event.preventDefault();
-
   query = $searchInput.value;
-
   $searchForm.reset();
 
-  return query;
-}
-
-$searchForm.addEventListener('submit', searchEventHandler);
-
-// Emtpy array to store Met object IDs in once gotten from the API
-var objIdArr = [];
-
-// Function to retreive object ID numbers from The Met api and save in objIdArr array:
-function getObjectIds(query) {
+  // Function to retreive object ID numbers from The Met api and save in objIdArr array:
   var queryXhr = new XMLHttpRequest();
   queryXhr.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/search?' + 'q=' + query + '&isOnView=true');
   queryXhr.responseType = 'json';
-
   queryXhr.addEventListener('load', function () {
     var responseObjectIds = queryXhr.response.objectIDs;
 
-    // Add Ids to objIdArr array:
+    // Add response IDs to objIdArr array:
     for (var id of responseObjectIds) {
       objIdArr.push(id);
     }
@@ -37,4 +27,4 @@ function getObjectIds(query) {
   queryXhr.send();
 }
 
-getObjectIds(query);
+$searchForm.addEventListener('submit', searchEventHandler);
