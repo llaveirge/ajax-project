9d84c2976d32fb787bd7e4e9159526a185f98cda
Lindsey Laveirge <llaveirge@gmail.com>
2022-01-05 17:32:59 -0700

    Listen for 'submit' event on the form and save the input value to a variable #1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $searchInput = document.getElementById('search');
+var $searchForm = document.getElementById('form');
+
+// Listen for events and save search value to a variable before resetting the form:
+
+var query;
+
+function searchEventHandler(event) {
+  event.preventDefault();
+
+  query = $searchInput.value;
+  return query;
+
+}
+
+$searchForm.addEventListener('submit', searchEventHandler);
