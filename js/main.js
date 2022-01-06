46cc4cc95fa8262038da5939957d3c747a6c158b
Lindsey Laveirge <llaveirge@gmail.com>
2022-01-05 18:39:00 -0700

    Create an empty array to store Met object IDs. Create a 'getObjectIds' function to create a new XMLHttpRequest() object, open() the object and send a 'GET' request to The Met museum's API with 'query' parameter value set to input value variable, and isOnView parameter value set to 'true'. Assign response type to 'json', and listen for 'load' event and add the response's 'objectIDs' property value to the Met object IDs array. Call the 'send()' method of the object. #1

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ function searchEventHandler(event) {
 }
 
 $searchForm.addEventListener('submit', searchEventHandler);
+
+// Emtpy array to store Met object IDs in once gotten from the API
+var objIdArr = [];
+
+// Function to retreive object ID numbers from The Met api and save in objIdArr array:
+function getObjectIds(query) {
+  var queryXhr = new XMLHttpRequest();
+  queryXhr.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/search?' + 'q=' + query + '&isOnView=true');
+  queryXhr.responseType = 'json';
+
+  queryXhr.addEventListener('load', function () {
+    var responseObjectIds = queryXhr.response.objectIDs;
+
+    // Add Ids to objIdArr array:
+    for (var id of responseObjectIds) {
+      objIdArr.push(id);
+    }
+
+  });
+  queryXhr.send();
+}
+
+getObjectIds(query);
