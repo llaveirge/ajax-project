6c90e444b2479ec1f0454fb9e04237f658060198
Lindsey Laveirge <llaveirge@gmail.com>
2022-01-07 14:41:37 -0700

    Verify the four randomized museum objects are saved to the data model and the museum object information array. #2

--- js/data.js
+++ js/data.js
@@ -6,3 +6,11 @@ var data = {
   saved: [],
   nextObjId: 1
 };
+
+function beforeunloadHandler(event) {
+
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', beforeunloadHandler);
