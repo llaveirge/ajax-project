a7284a6993d11b53ff537542389621939b164d64
Lindsey Laveirge <llaveirge@gmail.com>
2022-01-07 12:27:51 -0700

    Define a 'getObjectInfo' function that creates a 'new XMLHttpRequest()' object and save it to a variable, 'open()' the object to send a 'GET' request to The Met museum's API, listen for 'load' event and add object data from the response to a new object literal, and make sure that the object literal contains the appropriate information compared to the Ajax responses. #2

--- js/main.js
+++ js/main.js
@@ -36,15 +36,43 @@ var randomObjIds = [];
 // Empty array to store the four random museum objects and their information:
 // var randomObjInfo = [];
 
-/* Select and remove 4 random Met museum object IDs out of the 'objIdArr' array
+/* Select and remove 4 random Met museum object IDs from the 'objIdArr' array
 and add them to the 'randomObjIds' array: */
 function randomize(array) {
   for (var i = 0; i < 4; i++) {
     var randomIndex = Math.floor(Math.random() * array.length);
     randomObjIds.push(objIdArr.splice(randomIndex, 1));
   }
-  return randomObjIds;
-  // remove ^return statement
+  // console.log('Random object Ids:', randomObjIds);
 }
 
 randomize(objIdArr);
+
+// Testing random object Id from randomObjIds arr:
+var testId = randomObjIds[0];
+// console.log('random test ID number from array:', testId);
+
+// Retrieve randomized object data from Met API:
+function getObjectInfo(objectId) {
+  var dataXhr = new XMLHttpRequest();
+  dataXhr.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + objectId);
+  dataXhr.responseType = 'json';
+  dataXhr.addEventListener('load', function () {
+    var response = dataXhr.response;
+
+    var objectData = {
+      objImgUrl: response.primaryImageSmall,
+      objTitle: response.title,
+      objArtist: response.artistDisplayName,
+      objMeduim: response.medium,
+      objGallery: response.GalleryNumber,
+      objUrl: response.objectURL
+    };
+    return objectData;
+
+  });
+  dataXhr.send();
+
+}
+
+getObjectInfo(testId);
